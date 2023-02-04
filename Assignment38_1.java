376b05d129443762007d8f91d70cb657723868aa
anantmahadik <107344574+anantmahadik@users.noreply.github.com>
2023-02-04 17:18:24 +0530

    Add files via upload

---
+++ Assignment38_1.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Assignent38_1
+{
+    public static void main(String a[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter string :");
+        String str = sobj.nextLine();
+
+        Pattern ptr = new Pattern();
+        ptr.Pattern(str);
+    }
+}
+
+class Pattern
+{
+    public void Pattern(String str)
+    {
+        int iRow = str.length();
+        int iCol  = str.length();
+        char ch[] = str.toCharArray();
+        
+
+        for(int i = 1; i <= iRow; i++)
+        {
+            int iCnt = 0;
+            for(int j = 1; j <= iCol; j++)
+            {
+                System.out.print(ch[iCnt]+"\t");
+                iCnt++;
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Assignment38_5.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+class Assignent38_5
+{
+    public static void main(String a[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter string :");
+        String str = sobj.nextLine();
+
+        Pattern ptr = new Pattern();
+        ptr.Pattern(str);
+    }
+}
+
+class Pattern
+{
+    public void Pattern(String str)
+    {
+        int iRow = str.length();
+        int iCol  = str.length();
+        char ch[] = str.toCharArray();
+        
+
+        for(int i = 1; i <= iRow; i++)
+        {
+            int iCnt = 0;
+            for(int j = 1; j <= iCol; j++)
+            {
+                if(i >= j)
+                {
+                    System.out.print(ch[iCnt]+"\t");
+                    iCnt++;
+                }
+            }
+            System.out.println();
+        }
+        
+        for(int i = 1; i <= iRow; i++)
+        {
+            int iCnt = 0;
+            for(int j = 1; j < iCol; j++)
+            {
+                if(j >= i)
+                {
+                    System.out.print(ch[iCnt]+"\t");
+                    iCnt++;
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
