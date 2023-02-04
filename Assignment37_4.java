87db5bc05803e734e44c926f002b7776340ef142
anantmahadik <107344574+anantmahadik@users.noreply.github.com>
2023-02-04 17:17:49 +0530

    Add files via upload

---
+++ Assignment37_4.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Assignent37_4
+{
+    public static void main(String a[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter How many row :");
+        int iRow = sobj.nextInt();
+        
+        System.out.println("Enter How many Column :");
+        int iCol = sobj.nextInt();
+
+        Pattern ptr = new Pattern();
+        ptr.Pattern(iRow,iCol);
+    }
+}
+
+class Pattern
+{
+    public void Pattern(int iRow, int iCol)
+    {
+        
+        for(int i = 1; i <= iRow; i++)
+        {
+            for(int j = 1; j <= iCol; j++)
+            {
+                if(j % 2 == 0)
+                {
+                    System.out.print("#\t");
+                }
+                else
+                {
+                    System.out.print("*\t");
+                }
+                
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Assignment37_5.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Assignent37_5
+{
+    public static void main(String a[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter How many row :");
+        int iRow = sobj.nextInt();
+        
+        System.out.println("Enter How many Column :");
+        int iCol = sobj.nextInt();
+
+        Pattern ptr = new Pattern();
+        ptr.Pattern(iRow,iCol);
+    }
+}
+
+class Pattern
+{
+    public void Pattern(int iRow, int iCol)
+    {
+        
+        for(int i = 1; i <= iRow; i++)
+        {
+            for(int j = 1; j <= iCol; j++)
+            {
+                System.out.print(i+"\t");   
+            }
+            System.out.println();
+        }
+    }
+}
