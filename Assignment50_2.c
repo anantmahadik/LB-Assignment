c1868e71a430b76b490f3e0fd0c9dee0f22f14da
anantmahadik <107344574+anantmahadik@users.noreply.github.com>
2023-02-08 01:00:18 +0530

    Add files via upload

---
+++ Assignment50_2.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+typedef unsigned int UINT;
+
+UINT OffBit(UINT No)
+{
+    UINT iMask = 0XFFFFFDBF;
+
+    UINT Result = No & iMask ;
+
+    return Result;
+}
+
+int main()
+{
+    UINT Value = 0, Position1 = 0, Position2 = 0;
+    UINT iRet = 0;
+
+    printf("Enter number : \n");
+    scanf("%d",&Value);
+
+
+    iRet = OffBit(Value);
+
+    printf("Updated number is : %d\n",iRet);
+
+    return 0;
+}
+
+//  0000    0000    0000    0000    0000    0000    0000    0000
+
+//  1111    1111    1111    1111    1111    1101    1011    1111
+
+//  0XFFFFFDBF
